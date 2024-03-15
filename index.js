34967451d60e2307695b614e8b226bec0f96de4f
Rishabh0-0 <121654285+Rishabh0-0@users.noreply.github.com>
2024-03-15 22:06:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-var submitBtn = document.getElementsByClassName("btn-submit");
+const mainCard = document.querySelector(".main-card")
+const thankyouCard = document.querySelector(".thankyou-card")
+const submitBtn = document.querySelector(".btn-submit")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn-circle");
+
+submitBtn.addEventListener("click", () => {
+    mainCard.classList.add("d-none")
+    thankyouCard.classList.remove("d-none")
+});
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () =>{
+        rating.innerHTML = rate.innerHTML
+    })
+})
